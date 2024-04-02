dba10d4f1d11773517aba2cc309457e44d8bcecb
lucifayr <lucygschwantner@gmail.com>
2024-04-02 15:01:36 +0200

    wip: add basic websocket echo server

--- src/main/java/com/coopsnakeserver/app/App.java
+++ src/main/java/com/coopsnakeserver/app/App.java
@@ -1,13 +1,41 @@
 package com.coopsnakeserver.app;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import com.coopsnakeserver.app.sockets.EchoServer;
+import com.coopsnakeserver.app.sockets.TestSocket;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    static int PORT = 8765;
+
+    public static void main(String[] args) {
+        runServer();
+
+    }
+
+    public static void runServer() {
+
+        Server server = new Server("localhost", PORT, "/websockets", EchoServer.class);
+
+        try {
+
+            server.start();
+
+            var reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("Please press a key to stop the server.");
+            reader.readLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            server.stop();
+
+        }
+
     }
 }

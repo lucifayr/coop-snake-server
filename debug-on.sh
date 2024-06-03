826f4f17a9b65c0849c0b03303575e9ba4625057
lucifayr <lucygschwantner@gmail.com>
2024-06-03 13:17:36 +0200

    dev: unset empty vars in debug enable script

--- debug-on.sh
+++ debug-on.sh
@@ -6,12 +6,18 @@ export SNAKE_DEBUG=true
 
 if [ ! -z "$coords_file" ]; then
     export SNAKE_DEBUG_COORDS_FILE="$coords_file"
+else
+    unset SNAKE_DEBUG_COORDS_FILE
 fi
 
 if [ ! -z "$msg_in_latency" ]; then
     export SNAKE_DEBUG_MSG_IN_LATENCY="$msg_in_latency"
+else
+    unset SNAKE_DEBUG_MSG_IN_LATENCY
 fi
 
 if [ ! -z "$wrap_on_out_of_bounds" ]; then
     export SNAKE_DEBUG_WRAP_ON_OUT_OF_BOUNDS="$wrap_on_out_of_bounds"
+else
+    unset SNAKE_DEBUG_WRAP_ON_OUT_OF_BOUNDS
 fi

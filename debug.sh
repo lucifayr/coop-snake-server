4e93ea14b8d401cbc9c1836cdb4a162cfd47b42f
lucifayr <lucygschwantner@gmail.com>
2024-06-05 10:11:35 +0200

    dev: add newlines to debug script

--- debug.sh
+++ debug.sh
@@ -5,30 +5,30 @@ wrap_on_out_of_bounds="$3"
 export SNAKE_DEBUG=true
 
 if [ ! -z "$coords_file" ]; then
-    printf "-> coordinate replay file => $coords_file"
+    printf "-> coordinate replay file => $coords_file\n"
     export SNAKE_DEBUG_COORDS_FILE="$coords_file"
 else
-    printf "-> coordinate replay file => none"
+    printf "-> coordinate replay file => none\n"
     unset SNAKE_DEBUG_COORDS_FILE
 fi
 
 if [ ! -z "$msg_in_latency" ]; then
-    printf "-> artifical input message processing latency => $msg_in_latency"
+    printf "-> artifical input message processing latency => $msg_in_latency\n"
     export SNAKE_DEBUG_MSG_IN_LATENCY="$msg_in_latency"
 else
-    printf "-> artifical input message processing latency => disabled"
+    printf "-> artifical input message processing latency => disabled\n"
     unset SNAKE_DEBUG_MSG_IN_LATENCY
 fi
 
 if [ ! -z "$wrap_on_out_of_bounds" ]; then
-    printf "-> wrap around when out of bounds => true"
+    printf "-> wrap around when out of bounds => true\n"
     export SNAKE_DEBUG_WRAP_ON_OUT_OF_BOUNDS="$wrap_on_out_of_bounds"
 else
-    printf "-> wrap around when out of bounds => false"
+    printf "-> wrap around when out of bounds => false\n"
     unset SNAKE_DEBUG_WRAP_ON_OUT_OF_BOUNDS
 fi
 
 if [ -z "$coords_file" ] && [ -z "$msg_in_latency" ] && [ -z "$wrap_on_out_of_bounds" ]; then
-    printf "Debug mode disabled"
+    printf "Debug mode disabled\n"
     unset SNAKE_DEBUG
 fi

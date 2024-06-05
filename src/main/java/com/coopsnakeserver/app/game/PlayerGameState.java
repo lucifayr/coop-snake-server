e1a6c0b1a49ef777ccc63c5d6304ed2c8aa52f78
lucifayr <lucygschwantner@gmail.com>
2024-06-05 14:17:41 +0200

    feat: reset snake size when it is too large

--- src/main/java/com/coopsnakeserver/app/game/PlayerGameState.java
+++ src/main/java/com/coopsnakeserver/app/game/PlayerGameState.java
@@ -1,10 +1,13 @@
 package com.coopsnakeserver.app.game;
 
+import java.util.ArrayDeque;
+
 import org.springframework.web.socket.WebSocketSession;
 
 import com.coopsnakeserver.app.DevUtils;
 import com.coopsnakeserver.app.PlayerToken;
 import com.coopsnakeserver.app.game.frame.PlayerGameFrame;
+import com.coopsnakeserver.app.pojo.Coordinate;
 import com.coopsnakeserver.app.pojo.Player;
 import com.coopsnakeserver.app.pojo.SnakeDirection;
 
@@ -19,23 +22,37 @@ public class PlayerGameState {
     private GameSession session;
     private WebSocketSession ws;
 
+    private short initialSnakeSize;
+
     private Player player;
     private PlayerToken token;
 
     private PlayerGameFrame canonicalFrame;
 
     public PlayerGameState(GameSession parent, WebSocketSession ws, Player player, PlayerToken token,
-            short initialSankeSize) {
+            short initialSnakeSize) {
         this.session = parent;
         this.ws = ws;
         this.player = player;
         this.token = token;
 
-        createInitialFrame(initialSankeSize);
+        this.initialSnakeSize = initialSnakeSize;
+        createInitialFrame();
     }
 
     public void newCanonicalFrame(PlayerGameFrame frame) {
-        this.canonicalFrame = frame;
+        var maxSnakeSize = this.getBoardSize() * this.getBoardSize() - 1;
+        if (frame.getSnakeCoords().size() >= maxSnakeSize) {
+            var coords = frame.getSnakeCoords();
+            var snakeCoords = new ArrayDeque<Coordinate>(this.initialSnakeSize);
+            for (var i = 0; i < initialSnakeSize; i++) {
+                snakeCoords.addFirst(coords.pollFirst());
+            }
+
+            this.canonicalFrame = new PlayerGameFrame(snakeCoords, frame.getSnakeDirection(), frame.getFoodCoord());
+        } else {
+            this.canonicalFrame = frame;
+        }
     }
 
     public PlayerGameFrame canonicalFrame() {
@@ -58,14 +75,14 @@ public class PlayerGameState {
         return this.ws;
     }
 
-    private void createInitialFrame(short initialSankeSize) {
-        DevUtils.assertion(initialSankeSize > 0,
-                "Snake has to have more than 0 segemnts at the start of the game. Received " + initialSankeSize);
+    private void createInitialFrame() {
+        DevUtils.assertion(this.initialSnakeSize > 0,
+                "Snake has to have more than 0 segemnts at the start of the game. Received " + this.initialSnakeSize);
 
         short yOffset = player.getValue();
         var goLeft = yOffset % 2 == 0;
 
-        var snakeCoords = GameUtils.initialCoords(initialSankeSize, session.getBoardSize(), yOffset, goLeft);
+        var snakeCoords = GameUtils.initialCoords(this.initialSnakeSize, session.getBoardSize(), yOffset, goLeft);
         var foodCoord = GameUtils.findFoodCoord(snakeCoords.getFirst(), snakeCoords.stream().toList(),
                 this.session.getBoardSize());
 
